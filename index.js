9947bf9d2010ebca2f56de3e6c7bf6a3bd78d7ee
Emmanuelle Baron <emmanuellebaron1@gmail.com>
2021-08-17 09:18:45 +0200

    modificated test route

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-const express = require ("express");
+const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 
-
-
 const charactersRoutes = require("./routes/characters");
 app.use(charactersRoutes);
 const comicsRoutes = require("./routes/comics");
 app.use(comicsRoutes);
 
-app.get("/", (req,res)=>{
-    try {
-        res.status(200).json({message:"Hello"});
-    } catch (error) {
-       console.log(error.message)
-    }
-})
+app.get("/", async (req, res) => {
+  res.status(200).json({ message: "Hello" });
+});
 
-app.all("*",(req,res)=>{
-    res.status(404).json({message:"Page not found"});
-})
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Page not found" });
+});
 
-app.listen(3001,()=>{
-    console.log("Server started");
-})
+app.listen(process.env.PORT, () => {
+  console.log("Server started");
+});
